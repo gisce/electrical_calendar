b31a561eea007424a9d28c34239cf94d96e8d81f
Xavi Torelló <info@xaviertorello.cat>
2016-09-20 11:09:16 +0200

    Adapting the module loading and new version 0.4

--- electrical_calendar/__init__.py
+++ electrical_calendar/__init__.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'XaviTorello'
 
-import pkg_resources
+from .electrical import REECalendar
+from .omie import OMIECalendar
 
-from electrical_calendar import *
-
-
-#: Module version, as defined in PEP-0396.
-#__version__ = pkg_resources.get_distribution(__package__).version
-__version__ = 0.3
+__version__ = 0.4
