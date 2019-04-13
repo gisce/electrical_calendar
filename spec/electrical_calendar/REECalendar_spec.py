9db06279be3c6042042b747efcec71d335a86801
Xavi Torelló <info@xaviertorello.cat>
2019-04-13 11:50:54 +0200

    Add 2018 specs

--- spec/electrical_calendar/REECalendar_spec.py
+++ spec/electrical_calendar/REECalendar_spec.py
@@ -34,8 +34,16 @@ with description("A REE Calendar"):
             for idx, holiday in enumerate(expected_holidays):
                 assert holiday == expected_holidays[idx]
 
+        with it ('for 2018 will return the expected holidays'):
+            ree_cal = REECalendar()
+            holidays = ree_cal.holidays(2018)
+            expected_holidays = [(datetime.date(2018, 1, 1), 'New year'), (datetime.date(2018, 3, 30), 'Good Friday'), (datetime.date(2018, 5, 1), "Worker's Day"), (datetime.date(2018, 8, 15), 'Assumption of Mary to Heaven'), (datetime.date(
+                2018, 10, 12), 'National Day'), (datetime.date(2018, 11, 1), 'All Saints Day'), (datetime.date(2018, 12, 6), 'Constitution Day'), (datetime.date(2018, 12, 8), 'Immaculate Conception'), (datetime.date(2018, 12, 25), 'Christmas Day')]
 
             assert len(holidays) == len(expected_holidays)
+            for idx,holiday in enumerate(holidays):
+                assert holiday == expected_holidays[idx]
+
 
             for idx,holiday in enumerate(holidays):
                 assert holiday == expected_holidays[idx]
